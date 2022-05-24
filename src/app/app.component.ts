9b531ccc7661d15edf62151a98c6dde930cb7c8d
rBariles <rbariles@chattigo.com>
2022-05-24 14:55:35 -0300

    Init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sis-frontend';
+
+  navegations(index: number) {
+    let itemsIds: any = document.getElementsByClassName('itemMenu');
+
+    if(itemsIds) {
+      for(let i=0; i < itemsIds.length; i++) {
+        if(itemsIds[i].id == index.toString()) {
+          itemsIds[i].classList.add('item-select')
+        } else {
+          itemsIds[i].classList.remove('item-select');
+        }
+      };
+    }
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { PlanesComponent } from './components/planes/planes.component';
+import { FacturasComponent } from './components/facturas/facturas.component';
+import { ReportesComponent } from './components/reportes/reportes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientesComponent,
+    PlanesComponent,
+    FacturasComponent,
+    ReportesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
